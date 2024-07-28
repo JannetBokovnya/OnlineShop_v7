4e3866084f1093a0f9dda2c085517f93d7428b1f
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2024-07-28 19:16:45 +0300

    1 этап. Сконфигурирован IdentityServer так, что бы использовать Asp.Net.Core Identity который сохраняет своих пользователей в базе данных OnlineShopBd.Users и также IdentityServer сохраняет свою конфигурацию в базе данных OnlineShopBd.IdentityServer

---
+++ OnlineShop.IdentityServer/Config.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace OnlineShop.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("OnlineShop.Api"), //взаимодействие услуги между собой, что бы услуга а могла связаться с услугой в должна получить токен
+                new ApiScope("OnlineShop.Web"),  //будут заходить пользователи через web  интерфейс
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+               // клиент для взаимодействия услуг между собой
+                new Client
+                {
+                    ClientId = "test.client",
+                    ClientName = "Test client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "OnlineShop.Web",
+                        "OnlineShop.Api"
+                    }
+                },
+
+                 // внешний клиент, который будет заходить в приложение при помощи логина и пароля
+                new Client
+                {
+                    ClientId = "external",
+                    ClientName="External client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    RequireClientSecret = false,
+                     AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                         "OnlineShop.Web"
+                    }
+
+                },
+
+                //new Client
+                //{
+                //    ClientId = "movieClient",
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets =
+                //    {
+                //        new Secret("secret".Sha256())
+                //    },
+                //    AllowedScopes={ "OnlineShop.Web" }
+                //},
+            };
+    }
+}
